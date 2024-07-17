8d34a2300bd5c5e0ec93416de50fc1a810d0f0a3
kareemtaha-coder <engkareemoo20@gmail.com>
2024-07-17 15:56:15 +0300

    make courses loop

--- tamkeen/server/main.server.mjs
+++ tamkeen/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WOIMILMO.mjs";import"./chunk-GYO4SXUV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-HAVSKASM.mjs";import"./chunk-GYO4SXUV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};

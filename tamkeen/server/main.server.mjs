ab3b76a215ee63f282d16860496e95c90ea229dd
kareemtaha-coder <engkareemoo20@gmail.com>
2024-07-24 17:37:25 +0300

    edit responsive

--- tamkeen/server/main.server.mjs
+++ tamkeen/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-GFJBJ3LE.mjs";import"./chunk-N57VFFPL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-RVCNYTH4.mjs";import"./chunk-N57VFFPL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};

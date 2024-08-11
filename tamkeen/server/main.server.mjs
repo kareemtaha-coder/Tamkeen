9160b3e52f1e405e8e69d790e65ad2ae22a96f23
kareemtaha-coder <engkareemoo20@gmail.com>
2024-08-12 01:06:39 +0300

    update backend

--- tamkeen/server/main.server.mjs
+++ tamkeen/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SDEXVK2W.mjs";import"./chunk-VUI4J4LI.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-4JH7RLTT.mjs";import"./chunk-VUI4J4LI.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
